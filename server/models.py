5dab7bd1939743adf6cb5a84d320ac17773efb07
AnthonyLopez <anthonydlr66@gmail.com>
2024-07-22 15:57:55 -0400

    created class User

--- server/models.py
+++ server/models.py
@@ -2,3 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    # Fields
