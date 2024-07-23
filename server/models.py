22445edf0aa92e1e5f686a314e4d0769722d6336
AnthonyLopez <anthonydlr66@gmail.com>
2024-07-23 13:17:35 -0400

    test

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -14,3 +15,5 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     # Fields
+    id = db.column(db.Integer, primary_key=True)
+    username = db.column
