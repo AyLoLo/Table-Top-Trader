00af44e8df859471811ae096fa1656dedd205fa2
AnthonyLopez <anthonydlr66@gmail.com>
2024-07-24 14:51:47 -0400

    Setting up configs

--- backend/server/app.py
+++ backend/server/app.py
@@ -2,7 +2,7 @@ import ipdb
 import os
 
 from flask import Flask, make_response, jsonify, request, session
-from flask import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from flask_bcrypt import Bcrypt
 from flask_restful import Api, Resource
@@ -16,3 +16,4 @@ app.secret_key = app.config
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///boardgames.db'  # Update this to your database URI
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a random secret key
+
