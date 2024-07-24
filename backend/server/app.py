e095f92c072c076d2f9a884683c120f9ca82aa87
Alice <alicelin723@gmail.com>
2024-07-24 14:48:29 -0400

    fixed typos in models.py

--- backend/server/app.py
+++ backend/server/app.py
@@ -2,7 +2,7 @@ import ipdb
 import os
 
 from flask import Flask, make_response, jsonify, request, session
-from flask import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from flask_bcrypt import Bcrypt
 from flask_restful import Api, Resource
