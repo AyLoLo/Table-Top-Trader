26cce10f9ee76e2095b33847b85f2a6e749183db
AnthonyLopez <anthonydlr66@gmail.com>
2024-07-24 14:10:40 -0400

    sessions add neccessary lists and commits

--- backend/server/seed.py
+++ backend/server/seed.py
@@ -1,2 +1,20 @@
 from app import app
-from models import
+from models import db, User, Board_Game, Post, Review
+
+with app.app_context():
+
+    users = []
+    
+    posts = []
+
+    reviews = []
+
+    board_games = []
+
+    db.session.add_all[users]
+    db.session.add_all[posts]
+    db.session.add_all[reviews]
+    db.session.add_all[board_games]
+    db.session.commit()
+
+    print("Database Successfully Seeded")
