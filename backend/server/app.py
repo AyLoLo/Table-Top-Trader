966f82a4f55338705c8fe5c05f029b1e01e146b0
AnthonyLopez <anthonydlr66@gmail.com>
2024-07-25 13:35:06 -0400

    syntax errors fixed

--- backend/server/app.py
+++ backend/server/app.py
@@ -13,8 +13,7 @@ from flask_cors import CORS
 from models import db, User, Board_Game, Post, Review
 
 app = Flask(__name__)
-app.secret_key = '54e1d6161c01e73a319517df6dd892da'
-                  .join('dc1d3c022f35185032ef6b45947bcf0d')
+app.secret_key = '54e1d6161c01e73a319517df6dd892da'.join('dc1d3c022f35185032ef6b45947bcf0d')
 
 # ran python -c 'import secrets; print(secrets.token_hex())' in terminal
 # os.urandom(24)
@@ -71,7 +70,7 @@ class Users(Resource):
             }
             response_body.append(relationship)
         return make_response(jsonify(response_body), 200)
-    
+
     # User Signup
 
     @app.post('/users')
@@ -83,7 +82,7 @@ class Users(Resource):
         db.session.commit()
         session['user_id'] = new_user.user_id
         return new_user.to_dict(), 201
-    
+
     # Session Login/Logout
 
     @app.post('/login')
@@ -96,174 +95,184 @@ class Users(Resource):
             return user.to_dict(), 201
         else:
             return {"error": "Invalid username or password"}, 401 
-        
+
     @app.get('/current_session')
     def check_session():
         if logged_in():
             return get_current_user().to_dict(), 200
         else:
             return {}, 401
-        
+
     @app.post("/logout")
     def logout():
         session["user_id"] = None
         return {"message": "Successfully logged out"}, 204
 
+
 api.add_resource(Users, '/users')
 
+
 class Board_Games(Resource):
-     
-     def get(self):
-          board_games = Board_Game.query.all()
-          response_body = []
-          for board_game in board_games:
-               response_body.append(board_game.to_dict())
-          return make_response(jsonify(response_body), 200)
+
+    def get(self):
+        board_games = Board_Game.query.all()
+        response_body = []
+        for board_game in board_games:
+            response_body.append(board_game.to_dict())
+        return make_response(jsonify(response_body), 200)
+
 
 api.add_resource(Board_Games, "/boardgames")  
 
+
 class Posts(Resource):
-     
-     def get(self):
-          posts = Post.query.all()
-          response_body = []
-          for post in posts:
-               response_body.append(post.to_dict())
-          return make_response(jsonify(response_body), 200)
-     
-     def post(self):
-          try:
-               data = request.get_json()
-               # Boardgame_id and user_id neccessary?
-               new_post = Post(
-                    title = data.get('title'),
-                    user_id = data.get('user_id'),
-                    boardgame_id = data.get('boardgame_id'),
-                    description = data.get('description'),
-                    location = data.get('location'),
-                    data_created = data.get('date_created')
-                )
-               db.session.add(new_post)
-               db.session.commit()
-               response_body = new_post.to_dict()
-               return make_response(jsonify(response_body), 200)
-          except ValueError:
-               response_body  = {'errors': ['validation errors']}
-               return make_response(jsonify(response_body), 400)
+
+    def get(self):
+        posts = Post.query.all()
+        response_body = []
+        for post in posts:
+            response_body.append(post.to_dict())
+        return make_response(jsonify(response_body), 200)
+
+    def post(self):
+        try:
+            data = request.get_json()
+            # Boardgame_id and user_id neccessary?
+            new_post = Post(
+                title = data.get('title'),
+                user_id = data.get('user_id'),
+                boardgame_id = data.get('boardgame_id'),
+                description = data.get('description'),
+                location = data.get('location'),
+                data_created = data.get('date_created')
+            )
+            db.session.add(new_post)
+            db.session.commit()
+            response_body = new_post.to_dict()
+            return make_response(jsonify(response_body), 200)
+        except ValueError:
+            response_body = {'errors': ['validation errors']}
+            return make_response(jsonify(response_body), 400)
+
 
 api.add_resource(Posts, '/posts')
 
+
 class PostsByUser(Resource):
 
-     def get(self, username):
-          
-          user = User.query.filter_by(username = username).first()
-          if not user:
-              response_body = {"error": "User and their posts not found"}
-              return make_response(jsonify(response_body), 404)
-          response_body = [post.to_dict() for post in user.posts]
+    def get(self, username):
 
-          return make_response(jsonify(response_body), 200)
-     
-     @login_required
-     def patch(self, id):
-          post = Post.query.filter(Post.id == id).first()
-          if not post:
-               response_body = {"error": "Post not found"}
-               return make_response(jsonify(response_body), 404)
-          try:
-               data = request.get_json()
-               for key in data:
-                    setattr(post, key, data.get(key))
-               db.session.commit()
-               return make_response(jsonify(post.to_dict()), 202)
-          except ValueError:
-               response_body = {"errors": ['Validation Errors']}
-               return make_response(jsonify(response_body), 400)
-          
-     @login_required
-     def delete(self, id):
-          post = Post.query(filter(Post.id == id).first())
-          if not post:
-               response_body = {'error' : 'Post not found'}
-               return make_response(jsonify(response_body), 404)
-          db.session.delete(post)
-          db.session.commit()
-          response_body = {}
-          return make_response(jsonify(response_body), 204)
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            response_body = {"error": "User and their posts not found"}
+            return make_response(jsonify(response_body), 404)
+        response_body = [post.to_dict() for post in user.posts]
+
+        return make_response(jsonify(response_body), 200)
+
+    @login_required
+    def patch(self, id):
+        post = Post.query.filter(Post.id == id).first()
+        if not post:
+            response_body = {"error": "Post not found"}
+            return make_response(jsonify(response_body), 404)
+        try:
+            data = request.get_json()
+            for key in data:
+                setattr(post, key, data.get(key))
+                db.session.commit()
+            return make_response(jsonify(post.to_dict()), 202)
+        except ValueError:
+            response_body = {"errors": ['Validation Errors']}
+            return make_response(jsonify(response_body), 400)
+
+    @login_required
+    def delete(self, id):
+        post = Post.query(filter(Post.id == id).first())
+        if not post:
+            response_body = {'error': 'Post not found'}
+            return make_response(jsonify(response_body), 404)
+        db.session.delete(post)
+        db.session.commit()
+        response_body = {}
+        return make_response(jsonify(response_body), 204)
+
 
 # Rewrite this, BP
 api.add_resource(PostsByUser, '/<string:username>/posts/<int:id>')
 
 
 class Reviews(Resource):
-     
-     def get(self):
-          reviews = Review.query.all()
-          response_body = []
-          for review in reviews:
-               response_body.append(review.to_dict())
-          return make_response(jsonify(response_body), 200)
-     
-     def post(self):
-          try:
-               data = request.get_json()
-               new_review = Review(
-                    reviewer_id = data.get("reviewer_id"),
-                    subject_id = data.get("subject_id"),
-                    rating = data.get("rating"),
-                    comment = data.get("comment"),
-                    data_created = data.get("date_created")
-               )
-               db.session.add(new_review)
-               db.session.commit()
-               response_body = new_review.to_dict()
-               return make_response(jsonify(response_body), 200)
-          except ValueError:
-               response_body = {'errors' : ['validation errors']}
-               return make_response(jsonify(response_body), 400)
-          
+
+    def get(self):
+        reviews = Review.query.all()
+        response_body = []
+        for review in reviews:
+            response_body.append(review.to_dict())
+        return make_response(jsonify(response_body), 200)
+
+    def post(self):
+        try:
+            data = request.get_json()
+            new_review = Review(
+                reviewer_id = data.get("reviewer_id"),
+                subject_id = data.get("subject_id"),
+                rating = data.get("rating"),
+                comment = data.get("comment"),
+                data_created = data.get("date_created")
+            )
+            db.session.add(new_review)
+            db.session.commit()
+            response_body = new_review.to_dict()
+            return make_response(jsonify(response_body), 200)
+        except ValueError:
+            response_body = {'errors': ['validation errors']}
+            return make_response(jsonify(response_body), 400)
+
+
 api.add_resource(Reviews, '/reviews')
 
-class ReviewsByUser(Resource):
-     
-     def get(self, username):
-          
-          user = User.query.filter_by(username = username).first()
-          if not user:
-               response_body = {"error": "User and their reviews not found"}
-               return make_response(jsonify(response_body), 404)
-          response_body = [review.to_dict() for review in user.reviews]
-          return make_response(jsonify(response_body), 200)
-     
-     @login_required
-     def patch(self, id):
-        review = Review.query.filter(Review.id == id).first()
-        if not review :
-             response_body = {"error" :  "Review not found"}
-             return make_response(jsonify(response_body), 404)
-        try:
-             data = request.get_json()
-             for key in data:
-                  setattr(review, key, data.get(key))
-             db.session.commit()
-             return make_response(jsonify(review.to_dict()), 202)
-        except ValueError:
-             response_body = {'errors' : ["validation errors"]}
-             return make_response(jsonify(response_body), 400)
 
-     @login_required
-     def delete(self, id):
-          review = Review.query.filter(Review.id == id).first()
-          if not review :
-               response_body = {'error': 'Post not found'}
-               return make_response(jsonify(response_body) 404)
-          db.session.delete(review)
-          db.session.commit()
-          response_body = {}
-          return make_response(jsonify(response_body), 204)
-     
+class ReviewsByUser(Resource):
+
+    def get(self, username):
+
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            response_body = {"error": "User and their reviews not found"}
+            return make_response(jsonify(response_body), 404)
+        response_body = [review.to_dict() for review in user.reviews]
+        return make_response(jsonify(response_body), 200)
+
+    @login_required
+    def patch(self, id):
+        review = Review.query.filter(Review.id == id).first()
+        if not review:
+            response_body = {"error":  "Review not found"}
+            return make_response(jsonify(response_body), 404)
+        try:
+            data = request.get_json()
+            for key in data:
+                setattr(review, key, data.get(key))
+            db.session.commit()
+            return make_response(jsonify(review.to_dict()), 202)
+        except ValueError:
+            response_body = {'errors': ["validation errors"]}
+            return make_response(jsonify(response_body), 400)
+
+    @login_required
+    def delete(self, id):
+        review = Review.query.filter(Review.id == id).first()
+        if not review:
+            response_body = {'error': 'Post not found'}
+            return make_response(jsonify(response_body), 404)
+        db.session.delete(review)
+        db.session.commit()
+        response_body = {}
+        return make_response(jsonify(response_body), 204)
+
+
 api.add_resource(ReviewsByUser, '/<string:username>/reviews/<int:id>')
 
 if __name__ == '__main__':
-     app.run(port=7000, debug=True)
+    app.run(port=7000, debug=True)
