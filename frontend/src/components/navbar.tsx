476e45f2cd7c30434b9820dca430e8e461527d1f
Alice <alicelin723@gmail.com>
2024-08-01 19:10:49 -0400

    refactor input

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -3,10 +3,10 @@ import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 export const Navbar = (props: any) => {
   const {
-    isLoggedIn,
+    loggedInUser,
   } = props;
 
-  console.log(isLoggedIn);
+  console.log(loggedInUser);
   const [nav, setNav] = useState(false);
   const [postsSubNav, setPostsSubNav] = useState(false);
 
@@ -37,7 +37,7 @@ export const Navbar = (props: any) => {
         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           {!postsSubNav &&
             navItems
-              .filter(({ hideIfLoggedIn }) => isLoggedIn && !hideIfLoggedIn)
+              .filter(({ hideIfLoggedIn }) => loggedInUser && !hideIfLoggedIn)
               .map((item) => {
                 return (
                   <li
