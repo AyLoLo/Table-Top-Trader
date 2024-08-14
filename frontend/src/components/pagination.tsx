e9a345d7aafe2ad1ecf5bf866aab22e1f99253ee
AnthonyLopez <anthonydlr66@gmail.com>
2024-08-14 18:05:21 -0400

    useStates for page and posts per page in Pagination component, still need to call the function in return

--- frontend/src/components/pagination.tsx
+++ frontend/src/components/pagination.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
-export const Pagination = ({ handlePagination, postsPerPage, length}) => {
+const [currentPage, setCurrentPage] = useState(true)
+const [postsPerPage, setPostsPerPage] = useState(10)
+
+export const Pagination = (length : number, postsPerPage : number, handelePagination : any) => {
     const paginationNumbers = [];
 
     for (let i = 1; i <= Math.ceil(length / postsPerPage); i++) {
@@ -8,12 +11,18 @@ export const Pagination = ({ handlePagination, postsPerPage, length}) => {
     }
 
 
+    const handlePagination = (pageNumber) => {
+    setCurrentPage (pageNumber);
+    };
+
+
+
     // Implement a OnClick event for each page button. Call the handlePagiantion function with the appropriate page number
 
     return (
         <div>
             {paginationNumbers.map((pageNumber) => (
-                <button key={pageNumber} className={currentPage === pageNumber? 'active' : ''}>{pageNumber}</button>
+                <button key={pageNumber} className={currentPage === pageNumber ? 'active' : ''}>{pageNumber}</button>
             ))}
         </div>
     );
