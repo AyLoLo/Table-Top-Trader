5a934ac663af96cd740f96820f5c53e9c4fa842a
AnthonyLopez <anthonydlr66@gmail.com>
2024-08-14 18:09:07 -0400

    useState and effects for both posts and loading implemented, fetches Post data

--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import Post from '../components/post'
+import React, {useState, useEffect} from "react"
+import {PostProp} from "interfaces/PostProp"
+import {Post} from "components/post"
 
-function Posts({posts, loading}){
+const [posts, setPosts] = useState([]);
+const [loading, setLoading] = useState(false);
 
-    const post = posts.map(post => {
+useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`${URL}posts`)
+        const data = await response.json();
+        setPosts(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPosts()
+  }, []);
+
+export const Posts = (posts: PostProp[], loading : boolean) => {
+
+    const post = posts.map((post) => {
         return <Post post={post} loading={loading} key={post.id}/>
     })
 
@@ -13,5 +32,3 @@ function Posts({posts, loading}){
         </div>
     )
 }
-
-export default Posts;
