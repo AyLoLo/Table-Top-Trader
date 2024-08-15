e03c28447c66883d3d78e72695c1335a584a053c
AnthonyLopez <anthonydlr66@gmail.com>
2024-08-15 18:33:47 -0400

    imported URL to correctly fetch posts, imported pagination component to add to the return, experimenting with css

--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -1,34 +1,36 @@
 import React, {useState, useEffect} from "react"
-import {PostProp} from "interfaces/PostProp"
 import {Post} from "components/post"
+import {Pagination} from "components/pagination"
+import { URL } from "../constants"
 
-const [posts, setPosts] = useState([]);
-const [loading, setLoading] = useState(false);
 
-useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(`${URL}posts`)
-        const data = await response.json();
-        setPosts(data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchPosts()
-  }, []);
-
-export const Posts = (posts: PostProp[], loading : boolean) => {
-
-    const post = posts.map((post) => {
-        return <Post post={post} loading={loading} key={post.id}/>
+export const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+      const fetchPosts = async () => {
+        setLoading(true);
+        try {
+          const response = await fetch(`${URL}posts`)
+          const data = await response.json();
+          setPosts(data);
+          setLoading(false);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchPosts()
+    }, []);
+  
+    const post = posts.map((post : any) => {
+        return <Post post={post} loading={loading} key={post.post_id}/>
     })
 
     return(
-        <div>
-            {post}
+        <div className="bg-hero-background bg-center bg-no-repeat bg-over">
+          {post}
+          <Pagination posts={posts}/>
         </div>
     )
 }
