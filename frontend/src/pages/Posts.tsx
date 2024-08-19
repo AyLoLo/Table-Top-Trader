1b238dbfda0b89fae575106831fd83d7a6f74461
AnthonyLopez <anthonydlr66@gmail.com>
2024-08-19 17:00:04 -0400

    reaffirmed the key for when mapping post was the id of a post

--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -1,38 +1,44 @@
-import React, { useState, useEffect } from "react"
-import { Post } from "components/post"
-import { Pagination } from "components/pagination"
-import { URL } from "../constants"
+import React, {useState, useEffect} from "react";
+import {Post} from "components/post";
+import {Pagination} from "components/pagination";
+import { URL } from "../constants";
 
 
 const Posts = () => {
+  
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+  const [loading, setLoading] = useState(false);  
+  
   useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(`${URL}posts`)
-        const data = await response.json();
-        setPosts(data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchPosts()
-  }, []);
-
-  const post = posts.map((post: any) => {
-    return <Post post={post} loading={loading} key={post.post_id} />
+      const fetchPosts = async () => {
+        setLoading(true);
+        try {
+          const response = await fetch(`${URL}posts`)
+          const data = await response.json();
+          setPosts(data);
+          setLoading(false);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchPosts()
+    }, []);
+  
+  const post = posts.map((post : any) => {
+      return <Post post={post} loading={loading} key={post.id}/>
   })
 
   return (
-    <div className="bg-hero-background bg-center bg-no-repeat bg-over">
-      {post}
-      <Pagination posts={posts} />
+    <div>
+      <div>
+        {post}
+      </div>
+      <div className="border-4 border-gray-600">
+          <Pagination posts={posts}/>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default Posts;
