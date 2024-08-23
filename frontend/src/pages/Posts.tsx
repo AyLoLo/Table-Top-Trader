82a2ed897a73a83e3447b679f9d1f59f217e785d
AnthonyLopez <anthonydlr66@gmail.com>
2024-08-23 16:07:08 -0400

    added appropiate useStates for pages and updated fetchPosts to pass appropriate props down to component Pagination

--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -6,35 +6,46 @@ import { URL } from "../constants";
 
 const Posts = () => {
   
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);  
+  const [posts, setPosts] = useState([]); 
+  const [pageCount, setPageCount] = useState(0);
+  const [loading, setLoading] = useState(false); 
+  const [currentPage, setCurrentPage] = useState(1)
   
-  useEffect(() => {
-      const fetchPosts = async () => {
+  const fetchPosts = async () => {
         setLoading(true);
         try {
-          const response = await fetch(`${URL}posts`)
+          const response = await fetch(`${URL}posts?page=${currentPage}`)
           const data = await response.json();
-          setPosts(data);
+          setPosts(data.posts);
+          setPageCount(data.total_pages)
           setLoading(false);
         } catch (error) {
           console.log(error);
         }
-      };
-      fetchPosts()
-    }, []);
+  };
   
-  const post = posts.map((post : any) => {
-      return <Post post={post} loading={loading} key={post.id}/>
-  })
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  useEffect(() => {
+    fetchPosts();
+  }, [currentPage]);
+  
+
+  const handlePagination = (pageNumber : number) => {
+    setCurrentPage(pageNumber)
+  }
 
   return (
-    <div>
-      <div>
-        {post}
+    <div className="bg-hero-background bg-center bg-no-repeat bg-cover h-screen w-screen overflow-y-scroll">
+      <div className="grid grid-flow-row gap-10 pt-5">
+        {posts.map((post: any, index: number) =>
+          <Post post={post} key={post.id} />
+        )}
       </div>
-      <div className="border-4 border-gray-600">
-          <Pagination posts={posts}/>
+      <div className="border-4 border-blue-700 flex justify-center w-16 m-auto mt-5 bg-white">
+          <Pagination length={posts.length} pageCount={pageCount} currentPage={currentPage} handlePagination={handlePagination}/>
       </div>
     </div>
   );
