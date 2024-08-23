3a9d93faeef6161efe87cc8cf52c784e5bf92132
AnthonyLopez <anthonydlr66@gmail.com>
2024-08-23 16:07:46 -0400

    reworked css

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Home = () => {
-  return(
+export const Home = () => {
+  return (
     <div className="bg-hero-background bg-center bg-no-repeat bg-cover h-screen w-screen grid grid-cols-5 items-center justify-center">
       <div className="bg-white h-2/5 w-full col-span-2 border-gray-400 border-2 col-start-2 grid grid-rows-2">
         <h6 className="p-8 text-2xl text-green-600">Join The <span className="text-red-600 font-bold">Ultimate</span> Board Game Community!</h6>
@@ -10,5 +10,3 @@ const Home = () => {
     </div>
   );
 };
-
-export default Home;
