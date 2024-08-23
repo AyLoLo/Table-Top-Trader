0ac24af07ac610bf4aa08fb5fa779eaaa6060936
AnthonyLopez <anthonydlr66@gmail.com>
2024-08-23 16:03:14 -0400

    fleshed out logic, still in working progress

--- frontend/src/components/pagination.tsx
+++ frontend/src/components/pagination.tsx
@@ -2,29 +2,57 @@ import React, { useState, useEffect} from "react";
 import { PostProp } from "interfaces/PostProp"
 
 
-export const Pagination = (props : {posts : PostProp[]}) => {
-    const [currentPage, setCurrentPage] = useState(1)
-    const [postsPerPage, setPostsPerPage] = useState(10)
+export const Pagination = (props : any) => {
 
-    const handlePagination = (pageNumber : number) => {
-    setCurrentPage(pageNumber);
-    };
-    const paginationNumber = [];
-    const length = props.posts.length
+    const {
+        length,
+        currentPage,
+        pageCount
+    } = props;
+    
+    let paginationNumber = []
+    
+    let lastPage = pageCount
 
-    for (let i = 1; i <= Math.ceil(length / postsPerPage); i++) {
-        paginationNumber.push(i);
+    if (lastPage <= 1) {
+        return [];
     }
 
+    paginationNumber = [
+        ...(currentPage - 2 > 0 ? [currentPage - 2] : []),
+        ...(currentPage - 1 > 0 ? [currentPage - 1] : []),
+        currentPage,
+        ...(currentPage + 1 > 0 ? [currentPage + 1] : []),
+        ...(currentPage + 2 > 0 ? [currentPage + 2] : []),
+    ];
+
+    let size = paginationNumber.length
+
+    paginationNumber = [
+        ...(paginationNumber.length > 0 && paginationNumber[0] > 1 ? [1] : []),
+        ...(paginationNumber.length > 0 && paginationNumber[0] > 2 ? ["..."] : []),
+        ...paginationNumber,
+        ...(paginationNumber.length > 0 && paginationNumber[size - 1] + 1 < lastPage ? ["..."] : []),
+        ...(paginationNumber.length > 0 && paginationNumber[size - 1] < lastPage ? ["lastPage"] : [])
+    ];
+
+    const onNext = () => {
+        currentPage(currentPage + 1)
+    };
+
+    const onPrevious = () => {
+        currentPage(currentPage - 1)
+    }
+
+    const onPage = (e : any) => {
+        currentPage(Number(e.target.id))
+    }
+    
     return (
         <div>
-            {
-                paginationNumber.map((pageNumber) => (
-                    <button key={pageNumber} onClick={() => handlePagination(pageNumber)} className={currentPage === pageNumber ? 'active' : ''}>
-                        {pageNumber}
-                    </button>
-                ))
-            }
+            {currentPage > 1 && <a onClick={onPrevious}>Previous</a>}
+            {paginationNumber.map(num => <a onClick={onPage}>{num}</a>)}
+            {currentPage < lastPage && <a onClick={onNext}>Next</a>}
         </div>
     );
 };
