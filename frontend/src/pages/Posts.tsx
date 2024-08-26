36bd8ccc5d7e2a23b8d163d2e9a280d8333666ab
AnthonyLopez <anthonydlr66@gmail.com>
2024-08-26 17:04:35 -0400

    created a div that holds the postform component at the top of the page, passing down appropiate props posts

--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { Post } from "components/post";
 import { Pagination } from "components/pagination";
+import {PostForm} from "components/postform";
 import { URL } from "../constants";
 
 
@@ -38,19 +39,17 @@ const Posts = () => {
   }
 
   return (
-    <div className="bg-hero-background bg-center bg-no-repeat bg-cover h-screen w-screen overflow-y-scroll">
+    <div className="bg-hero-background bg-center bg-no-repeat bg-cover h-screen w-screen overflow-y-scroll grid gap-2">
+      <div>
+        <PostForm posts={posts} setPosts={setPosts}/>
+      </div>
       <div className="grid grid-flow-row gap-10 pt-5">
         {posts.map((post: any, index: number) =>
           <Post post={post} key={post.id} />
         )}
       </div>
-      <div className="border-4 border-blue-700 m-auto bg-white">
-          <Pagination 
-            length={posts.length} 
-            pageCount={pageCount} 
-            currentPage={currentPage} 
-            handlePagination={handlePagination}
-          />
+      <div className="border-4 border-blue-700 m-auto bg-white w-80 p-2 bottom-1">
+          <Pagination length={posts.length} setCurrentPage={setCurrentPage} pageCount={pageCount} currentPage={currentPage} handlePagination={handlePagination} />
       </div>
     </div>
   );
