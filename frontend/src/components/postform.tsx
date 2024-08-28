8620fb2a0d91c63480f93152c597b9cfe9102983
AnthonyLopez <anthonydlr66@gmail.com>
2024-08-28 17:05:38 -0400

    fleshed out the return for const Form, mapping out Post Form Inputs and importing component Input. Created Button with onClick function to hidePostModal.

--- frontend/src/components/postform.tsx
+++ frontend/src/components/postform.tsx
@@ -1,17 +1,20 @@
 import React, {useState, useEffect, ReactElement} from "react";
-import { FORM_INPUTS, URL } from "../constants";
+import { CREATE_POST_FORM_INPUTS, URL } from "../constants";
+import { Input } from "./input"
 import TTTUser from "../assets/TTTUser.png";
 import {FormProvider, useForm} from "react-hook-form"
 
 export const Form = (props : {
     addNewPost: Function;
     updateNewPost: Function;
+    handleOnChange: any;
     error: string;
 }): ReactElement<any> => {
     
     const {
         addNewPost,
         updateNewPost,
+        handleOnChange,
         error,
     } = props
 
@@ -21,20 +24,56 @@ export const Form = (props : {
     const hidePostModal = () => {
         setCreatePostModal(false)
     }
-    
-
 
     return (
-        // <>
-        //     <FormProvider {...methods}>
-        //         <form className="h-3/4 w-full">
-        //             {error && <p className="text-red-500">{error}</p>}
-        //         {FORM_INPUTS.filter((items.any)) =>}
-        //         </form>
-        //     </FormProvider>
-        // </>
+        <>
+            <FormProvider {...methods}>
+                <form className="h-3/4 w-full">
+                    {error && <p className="text-red-500">{error}</p>}
+                {CREATE_POST_FORM_INPUTS.map((inputItems: any) => 
+                    <div className="m-3" key={inputItems.label}>
+                        <Input handleOnChange={handleOnChange}  inputAttributes={inputItems}/>
+                    </div>)}
+                    <div className="flex items-center justify-end pt-6 border-t border-solid border-blue-200 rounded-b ">
+                        <button
+                            className="text-white bg-red-500 active:bg-red-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                            type="button"
+                            onClick={()  => hidePostModal()}
+                        >
+                            Close
+                        </button>
+                    </div>
+                </form>
+            </FormProvider>
+        </>
     )
+}
 
+
+
+export const addNewPost = async (e : any, currentUser : any) => {
+    
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("")
+    const [price, setPrice] = useState<number>(0)
+
+    const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+        switch (e.target.name) {
+            case "title":
+                setTitle(e.target.value);
+                break;
+            case "description":
+                setDescription(e.target.value);
+                break;
+            case "price":
+                setPrice(e.target.value);
+                break;
+            default:
+                break;
+        }
+    }
+    
     const {
         posts,
         setPosts
@@ -52,31 +91,30 @@ export const Form = (props : {
         console.log(error);
         }
     };
+    
+    e.preventDefault()
 
-    const addNewPost = async (e : any, currentUser : any) => {
-        e.preventDefault()
+    const newUserPost = {...newPost, 'user_id' : currentUser.user_id}
 
-        const newUserPost = {...newPost, 'user_id' : currentUser.user_id}
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
-        await fetch(`${URL}posts`, {
-            method: "POST",
-            body: JSON.stringify({newUserPost})
-        })
-        .then(response => {
-            if (response.ok){
-                response.json()
-                .then(newUserPost => setPosts([...posts, newUserPost]))
-                hidePostModal()
-                return response;
-            } else {
-                console.log("An error has occurred")
-                alert("An error has occurred")
-            }
-        })
-    }
+    await fetch(`${URL}posts`, {
+        method: "POST",
+        body: JSON.stringify({newUserPost})
+    })
+    .then(response => {
+        if (response.ok){
+            response.json()
+            .then(newUserPost => setPosts([...posts, newUserPost]))
+            hidePostModal()
+            return response;
+        } else {
+            console.log("An error has occurred")
+            alert("An error has occurred")
+        }
+    })
+}
 
     const updateNewPost = (e: any) => {
         setNewPost({...newPost, [e.target.id]: e.target.value})
@@ -100,4 +138,3 @@ export const Form = (props : {
         //     </div>
         // </div>
     )
-}
